a32f864332c6c13e05c0fd9838e9a310a25bfaa4
Nao Takegawa <nao.takegawa316@gmail.com>
2018-06-11 22:00:11 +0900

    create content of dashboard

--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 
 const Content = (props) => (
-  <div>
+  <div className="content-wrapper">
     This is going to be content
+    <section className="content-header">
+      <h1>
+        Dashboard
+        <small>Version 2.0</small>
+      </h1>
+      <ol className="breadcrumb">
+        <li><a href="#"><i className="fa fa-dashboard"></i> Home</a></li>
+        <li className="active">Dashboard</li>
+      </ol>
+    </section>
+
+    <section className="content">
+      // Content will be written here
+      <div className="row">
+        <div className="col-md-3 col-sm-6 col-xs-12">
+
+        </div>
+      </div>
+    </section>
   </div>
 );
 
