f1f2026a506a0c59c653df4f5d725003d8f8d58e
Nao Takegawa <nao.takegawa316@gmail.com>
2018-07-03 21:22:54 +0900

    use font awesome icon

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,23 +1,41 @@
 import React, { Component } from 'react';
 import '../../css/header.css';
 
-const Header = (props) => (
-  <header className="main-header">
-    <a href="#" className="logo"><h1>Cisum</h1></a>
-    <nav className="navbar navbar-static-top">
-      <a href="#" data-toggle="push-menu">
-        <span>Toggle Navigation</span>
-      </a>
-      <ul>
-        <li className="dropdown user user-menu">
-          <a href="#" className="dropdown-toggle" data-toggle="dropdown">
-            {/* image file will be shown here */}
-            <span className="hidden-xs">John Sculley</span>
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggleclose: false
+    }
+  }
+
+  render() {
+    const { toggleclose } = this.state;
+
+    const Header = (props) => (
+      <header className="main-header">
+        <a href="#" className="logo"><h1>Cisum</h1></a>
+        <nav className="navbar navbar-static-top">
+          <a href="#" className="sidebar-toggle" data-toggle="push-menu">
+            <span>Toggle Navigation</span>
           </a>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+          <ul>
+            <li className="dropdown user user-menu">
+              <a href="#" className="dropdown-toggle" data-toggle="dropdown">
+                {/* image file will be shown here */}
+                <span className="hidden-xs">John Sculley</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+
+    return (
+      <Header />
+    );
+  }
+}
+
 
 export default Header;
