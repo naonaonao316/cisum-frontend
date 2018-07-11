1e573c9ea22906dfeee493a048a195072dc788a4
Nao Takegawa <nao.takegawa316@gmail.com>
2018-07-11 22:14:25 +0900

    in the middle of using translate x

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,28 +5,39 @@ class Header extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      toggleclose: false
+      toggleClose: false
     }
   }
 
+  handleSidebarToggleClick = (event) => {
+    event.preventDefault();
+    console.log("toggle gets clicked!!")
+    this.setState({toggleClose: true})
+  }
+
   render() {
-    const { toggleclose } = this.state;
+    const { toggleClose } = this.state;
 
     const Header = (props) => (
       <header className="main-header">
-        <a href="#" className="logo"><span>Cisum</span></a>
-        <nav className="navbar navbar-static-top">
-          <a href="#" className="sidebar-toggle" data-toggle="push-menu">
+        <a href="#" className={`logo ${toggleClose ? "collapsed" : ""}`}>
+          <span className="logo-lg">Cisum</span>
+        </a>
+        <nav className={`navbar navbar-static-top ${toggleClose ? "collapsed" : ""}`}>
+          <a href="#" className="sidebar-toggle" data-toggle="push-menu" onClick={this.handleSidebarToggleClick}>
             <span className="sr-only">Toggle Navigation</span>
           </a>
-          <ul>
-            <li className="dropdown user user-menu">
-              <a href="#" className="dropdown-toggle" data-toggle="dropdown">
-                {/* image file will be shown here */}
-                <span className="hidden-xs">John Sculley</span>
-              </a>
-            </li>
-          </ul>
+          <div className="navbar-custom-menu">
+            <ul className="nav navbar-nav">
+              <li className="dropdown user user-menu">
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown">
+                  {/* image file will be shown here */}
+                  <img src="img/Bart_Simpson_200px.png" className="user-image"/>
+                  <span className="hidden-xs">John Sculley</span>
+                </a>
+              </li>
+            </ul>
+          </div>
         </nav>
       </header>
     );
