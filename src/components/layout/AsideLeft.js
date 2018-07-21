9c0f9dd7869b257dd5e9e88b64beb35ab122286c
Nao Takegawa <nao.takegawa316@gmail.com>
2018-07-21 14:12:37 +0900

    preserve changes

--- src/components/layout/AsideLeft.js
+++ src/components/layout/AsideLeft.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import '../../css/leftpane.css';
 
 const AsideLeft = (props) => (
   <aside className="main-sidebar">
-    This is going to be left pane
     <section className="sidebar">
       <div className="user-panel">
+        <div className="pull-left image">
+          <img src="img/Bart_Simpson_200px.png" className="img-circle"/>
+        </div>
+        <div className="pull-left info">
+          <p>John Sculley</p>
+          <a href="#">
+            <i className="fa fa-circle text-success"></i>Online
+          </a>
+        </div>
       </div>
       <form action="#" method="get" className="sidebar-form">
-
+        <div className="input-group">
+          <input type="text" name="q" className="form-control" placeholder="Search..." />
+          <span className="input-group-btn">
+            <button type="submit" name="search" id="search-btn" className="btn btn-flat">
+              <i className="fa fa-search"></i>
+            </button>
+          </span>
+        </div>
       </form>
       <ul className="sidebar-menu" data-widget="tree">
         <li className="header">Main Navigation</li>
